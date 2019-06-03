7546a974e174d890896181c92889ab007b7a9145
Mister24 <motivated24@hotmail.com>
2019-06-03 15:53:18 +0800

    add gitignore

---
+++ src/main/java/com/fayuan/thinkinginspring/BeanFactoryTest.java
@@ -0,0 +1,13 @@
+/**
+ * @Company Mister24.com Inc.
+ * @Copyright Copyright (c) 2016-2019 All Rights Reserved.
+ */
+package com.fayuan.thinkinginspring;
+
+/**
+ * @author mr.24
+ * @version Id: BeanFactoryTest, v 1.0 2019-06-03 15:49 Exp $$
+ */
+public class BeanFactoryTest {
+    @Test
+}
---
+++ src/main/java/com/fayuan/thinkinginspring/MyBean.java
@@ -0,0 +1,31 @@
+/**
+ * @Company Mister24.com Inc.
+ * @Copyright Copyright (c) 2016-2019 All Rights Reserved.
+ */
+package com.fayuan.thinkinginspring;
+
+/**
+ * @author mr.24
+ * @version Id: MyBean, v 1.0 2019-06-03 15:43 Exp $$
+ */
+public class MyBean {
+    private String str = "hello";
+
+    /**
+     * Getter method for property <tt>str</tt>.
+     *
+     * @return property value of str
+     */
+    public String getStr() {
+        return str;
+    }
+
+    /**
+     * Setter method for property <tt>str</tt>.
+     *
+     * @param str value to be assigned to property str
+     */
+    public void setStr(String str) {
+        this.str = str;
+    }
+}
