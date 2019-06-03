f8bb4c719db47f95b5654ab18b1afab153df7e53
Mister24 <motivated24@hotmail.com>
2019-06-03 22:29:42 +0800

    修改spring的xsd

--- src/main/java/com/fayuan/thinkinginspring/BeanFactoryTest.java
+++ src/main/java/com/fayuan/thinkinginspring/BeanFactoryTest.java
@@ -4,10 +4,23 @@
  */
 package com.fayuan.thinkinginspring;
 
+import org.junit.Test;
+import org.springframework.beans.factory.BeanFactory;
+import org.springframework.beans.factory.xml.XmlBeanFactory;
+import org.springframework.core.io.ClassPathResource;
+
 /**
  * @author mr.24
  * @version Id: BeanFactoryTest, v 1.0 2019-06-03 15:49 Exp $$
  */
+@SuppressWarnings("deprecation")
 public class BeanFactoryTest {
+
     @Test
+    public void testSimpleLoad() {
+        BeanFactory beanFactory = new XmlBeanFactory(new ClassPathResource("beanFactory.xml"));
+        MyBean myBean = (MyBean)beanFactory.getBean("myTestBean");
+
+        System.out.println(myBean.getStr());
+    }
 }
